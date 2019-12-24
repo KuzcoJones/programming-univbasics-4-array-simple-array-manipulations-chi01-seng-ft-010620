3ea78e7fe6d99828b34bf44b887ccbeffaa5c18a
Keenan Jones <keenanjones210@gmail.com>
2019-12-24 15:41:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,8 @@ def using_concat (array, array2)
   array.concat(array2)
 end
 
-def using_insert (array, element)
-  array.insert(element)
+def using_insert(array, element)
+  array[5].insert(element)
 end
 
 def using_uniq (array)
