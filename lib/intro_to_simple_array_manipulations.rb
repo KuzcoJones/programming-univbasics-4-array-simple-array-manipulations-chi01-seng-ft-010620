f3e8f28cf1d87867a41ce7783a165ee177b59510
Keenan Jones <keenanjones210@gmail.com>
2019-12-24 15:28:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,27 +26,27 @@ def shift_with_args (array, string)
   array.shift(2)
 end
 
-def using_concat (array, string)
-  array.push(string)
+def using_concat (array, array2)
+  array.concat(array2)
 end
 
-def using_insert (array, string)
-  array.push(string)
+def using_insert (array, element)
+  array.insert(element)
 end
 
-def using_uniq (array, string)
-  array.push(string)
+def using_uniq (array)
+  array.uniq
 end
 
-def using_flatten (array, string)
-  array.push(string)
+def using_flatten (array)
+  array.flatten
 end
 
 def using_delete (array, string)
-  array.push(string)
+  array.delete(string)
 end
 
-def using_delete_at (array, string)
-  array.push(string)
+def using_delete_at (array, integer)
+  array.delete(integer)
 end
 
