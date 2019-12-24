d5d8465ed9b13fc84e3750c2f4ad74efa8b5cb31
Keenan Jones <keenanjones210@gmail.com>
2019-12-24 15:38:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,7 +46,7 @@ def using_delete (array, string)
   array.delete(string)
 end
 
-def using_delete_at (array, 1)
-  array.delete(1)
+def using_delete_at (array, integer)
+  array.delete(integer)
 end
 
